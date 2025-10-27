cb29b11a8015fddd7c1169037f76ca0f01ace149
Remi <zestysourcitrus@gmail.com>
2025-10-27 12:45:00 -0400

    gtryjmhyuy6thgjiuhiy6iukvuioky6jioujhu7ki8y8uo89i0

---
+++ Example1.java
@@ -0,0 +1,30 @@
+//Remi-Laurent Guy | 10/27/25 | Loops Practice
+public class Example1 {
+public static void main(String[] args) {
+    
+
+
+
+
+}
+
+
+public static int countFactors(int num){
+//Return the number of factors in the number! :O
+//Btw, we know if something is a factor when we use modular division between 1 and a number
+//Use a while loop and determine if it's 0 or whatever
+
+
+
+
+}
+
+public static boolean isPrime(int num){
+//Determine if it's prime or not (I bet you couldn't tell from the method signature)
+//Use the other method (countFactors)
+
+
+
+}
+
+}
