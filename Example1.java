ba35fcb97e6a1b4ab2432b17f32a25796c9e40fa
Remi <zestysourcitrus@gmail.com>
2025-10-28 12:04:09 -0400

    hygt

--- Example1.java
+++ Example1.java
@@ -1,30 +1,47 @@
 //Remi-Laurent Guy | 10/27/25 | Loops Practice
 public class Example1 {
-public static void main(String[] args) {
+    public static void main(String[] args) {
+        
+    System.out.println(countFactors(24));
     
-
-
-
-
-}
-
-
-public static int countFactors(int num){
-//Return the number of factors in the number! :O
-//Btw, we know if something is a factor when we use modular division between 1 and a number
-//Use a while loop and determine if it's 0 or whatever
-
-
-
-
-}
-
-public static boolean isPrime(int num){
-//Determine if it's prime or not (I bet you couldn't tell from the method signature)
-//Use the other method (countFactors)
-
-
-
-}
-
-}
+    System.out.println(isPrime(47));
+    
+    }
+    
+    
+    public static int countFactors(int num){
+    //Return the number of factors in the number! :O
+    
+    int factors = 0;
+    
+    for(int count = 1; count <= num; count++){
+    
+    if(num % count == 0){
+        
+    factors++;
+        
+    }
+    
+    
+    }
+    
+    return factors;
+    
+    }
+    
+    public static boolean isPrime(int num){
+    //Determine if it's prime or not (I bet you couldn't tell from the method signature)
+    //Use the other method (countFactors)
+    
+    if(countFactors(num) == 2){
+    
+    return true;
+    
+    }
+    else{
+        
+    return false;
+    
+    }
+    }
+    }
