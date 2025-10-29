89953c5d1fb61ad8f796219e9fadf9302fc08e37
Remi <zestysourcitrus@gmail.com>
2025-10-29 12:25:26 -0400

    yry

---
+++ Hailstone.java
@@ -0,0 +1,33 @@
+//Remi-Laurent Guy | 10/29/25 | Guess the Number
+import java.util.*;
+public class Hailstone {
+    public static void main(String[] args) {
+        
+
+Scanner scanner = new Scanner(System.in);
+
+System.out.println("Enter a number from 1-200!!");
+
+int userNum = scanner.nextInt();
+
+
+while(userNum != 1){
+
+if(userNum % 2 == 0){
+
+userNum /= 2;
+
+}
+else{
+
+userNum = (userNum * 3) + 1;
+
+}
+
+System.out.println(userNum);
+
+
+}
+
+    }
+}
